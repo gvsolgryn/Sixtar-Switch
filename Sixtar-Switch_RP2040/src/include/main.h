e7a64be14130bf1bd18a8e80b987e4768711b541
GVSolgryn <tkdeh339@gmail.com>
2023-05-16 18:05:31 +0900

    nintendo switch joystick report (테스트 필요)

--- Sixtar-Switch_RP2040/src/include/main.h
+++ Sixtar-Switch_RP2040/src/include/main.h
@@ -8,20 +8,103 @@
 #include "tusb.h"
 #include "usb_descriptors.h"
 
-enum btnEnum {
-    BUTTON_1,
-    BUTTON_2,
-    BUTTON_3,
-    BUTTON_4,
-    BUTTON_COUNT
+enum {
+    PICO_GPIO_1_ON      = 0x00000001,
+    PICO_GPIO_2_ON      = 0x00000002,
+    PICO_GPIO_3_ON      = 0x00000004,
+    PICO_GPIO_4_ON      = 0x00000008,
+    PICO_GPIO_5_ON      = 0x00000010,
+    PICO_GPIO_6_ON      = 0x00000020,
+    PICO_GPIO_7_ON      = 0x00000040,
+    PICO_GPIO_8_ON      = 0x00000080,
+    PICO_GPIO_9_ON      = 0x00000100,
+    PICO_GPIO_10_ON     = 0x00000200,
+    PICO_GPIO_11_ON     = 0x00000400,
+    PICO_GPIO_12_ON     = 0x00000800,
+    PICO_GPIO_13_ON     = 0x00001000,
+    PICO_GPIO_14_ON     = 0x00002000,
+    PICO_GPIO_15_ON     = 0x00004000,
+    PICO_GPIO_16_ON     = 0x00008000,
+    PICO_GPIO_17_ON     = 0x00010000,
+    PICO_GPIO_18_ON     = 0x00020000,
+    PICO_GPIO_19_ON     = 0x00040000,
+    PICO_GPIO_20_ON     = 0x00080000,
+    PICO_GPIO_21_ON     = 0x00100000,
+    PICO_GPIO_22_ON     = 0x00200000,
+    PICO_GPIO_23_ON     = 0x00400000,
+    PICO_GPIO_24_ON     = 0x00800000,
+    PICO_GPIO_25_ON     = 0x01000000,
+    PICO_GPIO_26_ON     = 0x02000000,
+    PICO_GPIO_27_ON     = 0x04000000,
+    PICO_GPIO_28_ON     = 0x08000000,
+    PICO_GPIO_29_ON     = 0x10000000
+};
+
+enum BUTTON {
+    NS_BUTTON_Y         = 0x0001,
+    NS_BUTTON_B         = 0x0002,
+    NS_BUTTON_A         = 0x0004,
+    NS_BUTTON_X         = 0x0008,
+    NS_BUTTON_L         = 0x0010,
+    NS_BUTTON_R         = 0x0020,
+    NS_BUTTON_ZL        = 0x0040,
+    NS_BUTTON_ZR        = 0x0080,
+    NS_BUTTON_MINUS     = 0x0100,
+    NS_BUTTON_PLUS      = 0x0200,
+    NS_BUTTON_LCLICK    = 0x0400,
+    NS_BUTTON_RCLICK    = 0x0800,
+    NS_BUTTON_HOME      = 0x1000,
+    NS_BUTTON_CAPTURE   = 0x2000
+};
+
+enum HAT {
+    NS_HAT_UP             = 0x00,
+    NS_HAT_UP_RIGHT       = 0x01,
+    NS_HAT_RIGHT          = 0x02,
+    NS_HAT_DOWN_RIGHT     = 0x03,
+    NS_HAT_DOWN           = 0x04,
+    NS_HAT_DOWN_LEFT      = 0x05,
+    NS_HAT_LEFT           = 0x06,
+    NS_HAT_UP_LEFT        = 0x07,
+    NS_HAT_NEUTRAL        = 0x08
+};
+
+enum HAT_BTN {
+    NS_HAT_BTN_UP         = 0b0001,
+    NS_HAT_BTN_RIGHT      = 0b0010,
+    NS_HAT_BTN_DOWN       = 0b0100,
+    NS_HAT_BTN_LEFT       = 0b1000
+};
+
+enum NS_STICK {
+    NS_STICK_MIN          = 0,
+    NS_STICK_NEUTRAL      = 128,
+    NS_STICK_MAX          = 255
 };
 
 typedef struct {
-    uint8_t HAT_UP;
-    uint8_t HAT_DOWN;
-    uint8_t HAT_LEFT;
-    uint8_t HAT_RIGHT;
-} hat_status;
+    uint16_t Button;
+    uint8_t Hat;
+    uint8_t LX;
+    uint8_t LY;
+    uint8_t RX;
+    uint8_t RY;
+    uint8_t VendorSpec;
+} USB_JoystickReport_Input_t;
+
+enum {
+    GPIO_1,
+    GPIO_2,
+    GPIO_3,
+    GPIO_4,
+    GPIO_5,
+    GPIO_6,
+    GPIO_7,
+    GPIO_8,
+    GPIO_9,
+    GPIO_10,
+    BUTTON_COUNT
+};
 
 typedef struct {
     uint16_t Y;
@@ -38,10 +121,9 @@ typedef struct {
     uint16_t RCLICK;
     uint16_t HOME;
     uint16_t CAPTUR;
-} button_status;
+} Button_Status;
 
-struct Hat
-{
+typedef struct {
     uint8_t UP;
     uint8_t UP_RIGHT;
     uint8_t RIGHT;
@@ -51,27 +133,11 @@ struct Hat
     uint8_t LEFT;
     uint8_t UP_LEFT;
     uint8_t NEUTRAL;
-} hat = {
-    0x00,
-    0x01,
-    0x02,
-    0x03,
-    0x04,
-    0x05,
-    0x06,
-    0x07,
-    0x08
-};
+} Hat_Status;
 
-struct HatButton
-{
+typedef struct {
     uint8_t UP;
     uint8_t RIGHT;
     uint8_t DOWN;
     uint8_t LEFT;
-}hat_btn = {
-    0b0001,
-    0b0010,
-    0b0100,
-    0b1000
-};
+} Hat_Button_Status;
--- Sixtar-Switch_RP2040/src/main.c
+++ Sixtar-Switch_RP2040/src/main.c
@@ -10,36 +10,17 @@
 //--------------------------------------------------------------------+
 // USB HID
 //--------------------------------------------------------------------+
-button_status btn_stat = {
-    .A = 0,
-    .B = 0,
-    .X = 0,
-    .Y = 0,
-    .CAPTUR = 0,
-    .HOME = 0,
-    .L = 0,
-    .LCLICK = 0,
-    .MINUS = 0,
-    .PLUS = 0,
-    .R = 0,
-    .RCLICK = 0
-};
+Button_Status btn_stat;
+Hat_Status hat_stat;
+Hat_Button_Status hat_btn_stat;
+
+USB_JoystickReport_Input_t ns_report;
+hid_gamepad_report_t report;
 
 static void send_hid_report(uint8_t report_id, uint32_t btn) {
     // skip if hid is not ready yet
     if (!tud_hid_ready())
         return;
-
-    hid_gamepad_report_t report = {
-        .x			= 0,
-        .y			= 0,
-        .z			= 0,
-        .rx			= 0,
-        .ry			= 0,
-        .rz			= 0,
-        .hat		= 0,
-        .buttons	= 0
-    };
 
     switch(btn) {
         case 0: {
@@ -54,25 +35,28 @@ static void send_hid_report(uint8_t report_id, uint32_t btn) {
             break;
         }
 
-        default : {
-            report.x = 0;
-            report.y = 0;
-            report.z = 0;
-            report.rx = 0;
-            report.ry = 0;
-            report.rz = 0;
-            report.hat = 0;
-            report.buttons = 0;
+        default: {
+            report.x        = 0;
+            report.y        = 0;
+            report.z        = 0;
+            report.rx       = 0;
+            report.ry       = 0;
+            report.rz       = 0;
+            report.hat      = 0;
+            report.buttons  = 0;
 
             break;
         }
     }
 
-    report.buttons += btn_stat.A == 0 ? GAMEPAD_BUTTON_A : 0x0000;
-    report.buttons += btn_stat.B == 0 ? GAMEPAD_BUTTON_B : 0x0000;
-    report.buttons += btn_stat.X == 0 ? GAMEPAD_BUTTON_X : 0x0000;
-    report.buttons += btn_stat.Y == 0 ? GAMEPAD_BUTTON_Y : 0x0000;
+    report.buttons += btn_stat.Y == 0 ? NS_BUTTON_Y : 0x0000;
+    report.buttons += btn_stat.B == 0 ? NS_BUTTON_B : 0x0000;
+    report.buttons += btn_stat.A == 0 ? NS_BUTTON_A : 0x0000;
+    report.buttons += btn_stat.X == 0 ? NS_BUTTON_X : 0x0000;
+    report.buttons += btn_stat.L == 0 ? NS_BUTTON_L : 0x0000;
+    report.buttons += btn_stat.R == 0 ? NS_BUTTON_R : 0x0000;
 
+    tud_hid_report(report_id, &ns_report, sizeof(ns_report));
     tud_hid_report(report_id, &report, sizeof(report));
 }
 
@@ -89,14 +73,16 @@ static void update_input() {
 
     uint32_t const btn = board_button_read();
 
-    btn_stat.A = gpio_get(BUTTON_1);
-    btn_stat.B = gpio_get(BUTTON_2);
-    btn_stat.X = gpio_get(BUTTON_3);
-    btn_stat.Y = gpio_get(BUTTON_4);
+    btn_stat.Y = gpio_get(GPIO_1);
+    btn_stat.B = gpio_get(GPIO_2);
+    btn_stat.A = gpio_get(GPIO_3);
+    btn_stat.X = gpio_get(GPIO_4);
+    btn_stat.L = gpio_get(GPIO_5);
+    btn_stat.R = gpio_get(GPIO_6);
 
     board_led_write((bool)btn);
 
-    gpio_get_dir(BUTTON_1) ? board_led_off() : board_led_on();
+    gpio_get(GPIO_1) ? board_led_off() : board_led_on();
 
     send_hid_report(REPORT_ID_GAMEPAD, btn);
 
@@ -111,6 +97,12 @@ void init() {
 
         gpio_pull_up(i);
     }
+
+    memset(&btn_stat, 0, sizeof(Button_Status));
+    memset(&hat_stat, 0, sizeof(Hat_Status));
+    memset(&hat_btn_stat, 0, sizeof(Hat_Button_Status));
+    memset(&ns_report, 0, sizeof(USB_JoystickReport_Input_t));
+    memset(&report, 0, sizeof(hid_gamepad_report_t));
 
     return;
 }
